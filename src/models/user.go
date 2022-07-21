d2c57b0613900ef40fbf87bbff322697ee1fd990
Pankaj Dhundhara <pankajdhundhara@Pankajs-MacBook-Pro.local>
2022-07-21 17:00:59 +0530

    resolved all

--- src/models/user.go
+++ src/models/user.go
@@ -3,22 +3,22 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	Id            primitive.ObjectID `json:"_id" bson:"_id"`
-	Name          string             `json:"name" bson:"name"`
-	Email         string             `json:"email" bson:"email"`
-	Balance       float64            `json:"balance" bson:"balance"`
-	Type          string             `json:"type" bson:"type"`
-	AccountType   string             `json:"accountType" bson:"accountType"`
-	AccountNumber   string             `json:"accountNumber" bson:"accountNumber"`
-	Status        string             `json:"status" bson:"status"`
-	BankName      string             `json:"bankName" bson:"bankName"`
-	Country       string             `json:"country" bson:"country"`
-	Currency      string             `json:"currency" bson:"currency"`
-	Customer      interface{}        `json:"customer" bson:"customer"`
-	Fingerprint   string             `json:"fingerprint" bson:"fingerprint"`
-	Last4         string             `json:"last4" bson:"last4"`
-	History       []primitive.ObjectID        `json:"history" bson:"history"`
-	RoutingNumber string             `json:"routing_number" bson:"routing_number"`
+	Id            primitive.ObjectID   `json:"_id" bson:"_id"`
+	Name          string               `json:"name" bson:"name"`
+	Email         string               `json:"email" bson:"email"`
+	Balance       float64              `json:"balance" bson:"balance"`
+	Type          string               `json:"type" bson:"type"`
+	AccountType   string               `json:"accountType" bson:"accountType"`
+	AccountNumber string               `json:"accountNumber" bson:"accountNumber"`
+	Status        string               `json:"status" bson:"status"`
+	BankName      string               `json:"bankName" bson:"bankName"`
+	Country       string               `json:"country" bson:"country"`
+	Currency      string               `json:"currency" bson:"currency"`
+	Customer      interface{}          `json:"customer" bson:"customer"`
+	Fingerprint   string               `json:"fingerprint" bson:"fingerprint"`
+	Last4         string               `json:"last4" bson:"last4"`
+	History       []primitive.ObjectID `json:"history" bson:"history"`
+	RoutingNumber string               `json:"routing_number" bson:"routing_number"`
 
 	//TODO: Implement logic for auth
 	Password     string `json:"password" bson:"password"`
